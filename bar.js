d149ecfd9d4beebd4802fa19868e7d337008a4d2
zhaoyunpeng <zhaoyunpeng@lilithgames.com>
2019-11-22 19:22:13 +0800

    feat: commit

---
+++ bar.js
@@ -0,0 +1,48 @@
+// bar.js
+const server = require("umi-server");
+const http = require("http");
+const { createReadStream } = require("fs");
+const { join, extname } = require("path");
+
+const root = join(__dirname, "dist");
+const render = server({
+	root,
+});
+const headerMap = {
+	".js": "text/javascript",
+	".css": "text/css",
+	".jpg": "image/jpeg",
+	".png": "image/jpeg",
+};
+
+http
+	.createServer(async (req, res) => {
+		const ext = extname(req.url);
+		const header = {
+			"Content-Type": headerMap[ext] || "text/html",
+		};
+		res.writeHead(200, header);
+
+		if (!ext) {
+			// url render
+			const ctx = {
+				req,
+				res,
+			};
+			const { ssrHtml } = await render(ctx);
+			res.write(ssrHtml);
+			res.end();
+		} else {
+			// static file url
+			const path = join(root, req.url);
+			const stream = createReadStream(path);
+			stream.on("error", error => {
+				res.writeHead(404, "Not Found");
+				res.end();
+			});
+			stream.pipe(res);
+		}
+	})
+	.listen(3000);
+
+console.log("http://localhost:3000");
